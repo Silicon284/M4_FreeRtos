49dbed08e38029941fe6817bde8e60332f3feaa9
Silicon284 <152059607+Silicon284@users.noreply.github.com>
2026-05-04 22:36:56 +0530

    Full project compiled

--- Single_Core_SmartCar/application/source/main.c
+++ Single_Core_SmartCar/application/source/main.c
@@ -7,6 +7,10 @@
 #include <stdint.h>
 #include "stm32h7xx_hal.h"
 #include "stm32h7xx_nucleo.h"
+#include "Bluetooth_Console.h"
+#include "IMU_Control.h"
+#include "MotorControl.h"
+#include "Ultrasonic_Sensor.h"
 
 SemaphoreHandle_t xMutex, xSemaphore;
 
@@ -104,6 +108,55 @@ void BoardStartUp (void) {
   HAL_PWREx_EnterSTOPMode(PWR_MAINREGULATOR_ON, PWR_STOPENTRY_WFE, PWR_D2_DOMAIN);
   /* Clear HSEM flag */
   __HAL_HSEM_CLEAR_FLAG(__HAL_HSEM_SEMID_TO_MASK(0));
+
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+  HAL_MspInit();
+  /* USER CODE BEGIN Init */
+
+  /* Initialize leds */
+  BSP_LED_Init(LED_GREEN);
+  BSP_LED_Init(LED_YELLOW);
+  BSP_LED_Init(LED_RED);
+
+  /* Initialize USER push-button, will be used to trigger an interrupt each time it's pressed.*/
+  BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
+
+  // STM32H7 specific: Ensure USART2 is in the correct power domain
+  __HAL_RCC_D2SRAM1_CLK_ENABLE();
+  __HAL_RCC_D2SRAM2_CLK_ENABLE();
+  __HAL_RCC_D2SRAM3_CLK_ENABLE();
+  
+  // Wait for USART2 clock to stabilize
+  HAL_Delay(1);
+  Terminal_Console_Init();
+  MX_TIM3_Init();
+  //HAL_Delay(1);
+  MX_TIM4_Init();
+
+  //MX_I2C4_Init();
+  //BlueTooth_Console_Init();
+
+  // Initialize Motor Control System
+ // MotorControl_Init();
+  //MotorControl_ADC_Init();
+  
+  char motor_init_msg[] = "Motor Control and ADC Initialized!\r\n";
+  Terminal_Display(motor_init_msg);
+
+ HAL_StatusTypeDef statusI2C = HAL_OK;
+
+  char timer_init_msg[100];
+  uint8_t IMUAdd = 0x68<<1;
+  uint16_t MemAddSize = 1;
+  uint8_t pData = 0x04;
+  uint8_t tData[2];
+  uint16_t Size = 1;
+  uint32_t Timeout = 100;
+  int16_t temperature_raw = 0;
+  float_t temperature = 0.0;
+
+  //HAL_I2C_Mem_Write(&hi2c1, IMUAdd, 0x6B, MemAddSize, &pData,  Size,  Timeout);
 }
 
 void main(void) {
